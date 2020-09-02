dc12cc0f31ec23e1d616dabca7206e1b63689d3b
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2020-09-02 21:54:16 +0530

    phoneno and email validation added

--- src/Utils.js
+++ src/Utils.js
@@ -1,5 +1,26 @@
 export function isValidUserName(e){
-    
 
+    if(!e.target.value.length){
+        alert("username cannot be empty")
+        return false
+    }
+    return true
+}
+
+export function isValidEmail(e){
+
+    var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+    if(!pattern.test(e.target.value)){
+        return false
+    }
+    return true
+}
+
+export function isValidPhoneNumber(e){
+
+    var phoneno = /^\d{10}$/;
+    if(isNaN(e.target.value)||(!phoneno.test(e.target.value))){
+        return false
+    }
     return true
 }
