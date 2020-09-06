7ce9aed3ae3deb4ba60a6ee9146dd6c72ca8e9fd
keerthanacse13@gmail.com <keerthanacse13@gmail.com>
2020-09-06 22:18:27 +0530

    useEffect try with Background image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React , { useState, useEffect } from 'react'
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom"
 // import Prelogin from "./screens/prelogin/prelogin"
 // import Homepage from "./screens/postlogin/homepage"
 import Login from "./components/prelogin/login"
 import Register from "./components/prelogin/register"
 import Dashboard from "./components/postlogin/dashboard"
+import desktopImage from "./images/strawberry.jpg"
+import mobileImage from "./images/strawberry.jpg"
+import './App.css';
+
+
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const imageUrl = window.innerWidth >= 650 ? desktopImage : mobileImage;
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
+    
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+        window.removeEventListener('resize', handleWindowResize);
+    }
+}, []);
   return (
     <div >
       <BrowserRouter> 
@@ -17,6 +36,12 @@ function App() {
           <Redirect to={Login} />
         </Switch>  {/* note the usage of Switch */}
       </BrowserRouter>
+      <div className="App" style={{backgroundImage: `url(${imageUrl})` }}>
+            <div className="App-content">
+                <h1>Pineapples</h1>
+                <p>They are good</p>
+            </div>
+        </div>
     </div>
   );
 }
